d6b4ef059714aa8d0723f800f891b1a54caeffb6
Regan Gilbert <regan.m.gilbert@gmail.com>
2017-11-30 18:30:54 -0700

    added skills

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  def home
+<<<<<<< HEAD
+    @posts = Blog.all
+    @skills = Skill.all
+=======
+  	@posts =Blog.all
+>>>>>>> d4365ddaab8d24477f031fec232193db26cdaf0b
+  end
+
+  def about
+  end
+
+  def contact
+  end
+<<<<<<< HEAD
+end
+=======
+end
+>>>>>>> d4365ddaab8d24477f031fec232193db26cdaf0b
