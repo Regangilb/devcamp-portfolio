f0856c8c4f5c2cb3b270b0db2264e91b6abd2fca
Regan Gilbert <regan.m.gilbert@gmail.com>
2017-12-26 11:06:00 -0700

    Integrated Technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,21 +4,20 @@
   )
 end
 
-puts "3 Topics created”
-
-
-
 puts "3 Topics created"
+
 
 10.times do |blog|
   Blog.create!(
-    title: "My Blog Post #{blog}",
+   title: "My Blog Posts #{blog}",
     body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
     topic_id: Topic.last.id
   )
 end
 
 puts "10 blog posts created"
+
+
 
 5.times do |skill|
   Skill.create!(
@@ -56,3 +55,12 @@ end
 end
 
 puts "9 portfolio items created"
+
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
